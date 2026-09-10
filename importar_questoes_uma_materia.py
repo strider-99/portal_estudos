33ce47ad875e418c7018fda402824c783bb778b3
Adilson <adilsonjunior.001@gmail.com>
2026-09-09 21:19:00 -0300

    Nova leva de apostilas: Administração Pública, Administração Geral e RH.

--- importar_questoes_uma_materia.py
+++ importar_questoes_uma_materia.py
@@ -7,7 +7,7 @@ load_dotenv()
 client = MongoClient(os.getenv('MONGO_URI'))
 db = client.get_default_database()
 
-with open('data/questoes_informatica_windows.json', 'r') as f:
+with open('data/questoes_matematica_modulo1.json', 'r') as f:
     dados = json.load(f)
 
 for item in dados:
